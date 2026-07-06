110060a171bfefb2b629a3c161dedd93a82ef7da
colin-gourlay <you@example.com>
2026-07-06 11:28:31 +0100

    chore(repo): remove dotnet automation solution

--- automation/dotnet/integrations/SundownMedia.Integration.Spotify/Models/SpotifyTrack.cs
+++
@@ -1,7 +0,0 @@
-// <copyright file="SpotifyTrack.cs" company="SundownMedia">
-// Copyright (c) SundownMedia. All rights reserved.
-// </copyright>
-
-namespace SundownMedia.Integration.Spotify.Models;
-
-public sealed record SpotifyTrack(string Id, string Artist, string Title, string? Isrc, string? Label);
